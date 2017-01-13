fb3692b896c519274313d45dd1e1e3158c616ca5
Anthony Zeng <anthonyzeng99@gmail.com>
2017-01-13 11:01:13 -0500

    added networking code

--- spyfall_client.c
+++ spyfall_client.c
@@ -1,38 +1,17 @@
-#include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/in.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #include "spyfall_client.h"
 
 
-int client_connect( char *host ) {
-  int sd, i;
-  
-  sd = socket( AF_INET, SOCK_STREAM, 0 );
-  error_check( sd, "client socket" );
-  
-  struct sockaddr_in sock;
-  sock.sin_family = AF_INET;
-  inet_aton( host, &(sock.sin_addr));
-  sock.sin_port = htons(9001);
-  
-  printf("[client] connecting to: %s\n", host );
-  i = connect( sd, (struct sockaddr *)&sock, sizeof(sock) );
-  error_check( i, "client connect");
-  
-  return sd;
-}
-
 int main() {
-  printf("Welcome to Spyfall\n");
-  printf("Please enter the IP address of the Spyfall server:");
-
-  int ip_address = fgets( buffer, sizeof(buffer), stdin );
 
 }
--- spyfall_networking.h
+++ spyfall_networking.h
@@ -1,4 +1,14 @@
 #ifndef SPYFALL_NETWORKING_H
 #define SPYFALL_NETWORKING_H
 
+#define MESSAGE_BUFFER_SIZE 1000
+
+void error_check( int i, char *s );
+
+int server_setup();
+int server_connect(int sd);
+
+int client_connect( char *host );
+
+
 #endif
