cd8ccbb71ad10f20b79c2af0104827e0b62e8c2d
Anthony Zeng <anthonyzeng99@gmail.com>
2017-01-23 10:46:26 -0500

    server player struct

--- spyfall_server.c
+++ spyfall_server.c
@@ -11,30 +11,42 @@
 
 #include "networking.h"
 
-void process( char * s );
-void sub_server( int sd );
+void sub_server(int sd, int player_id);
+void process (char *s);
 
-int players = 0;
-int player_ids[5];
+int USERNAME_SIZE;
+
+
+struct player_info {
+  int usrid;
+  char username[20];
+  int spy; 
+};
+
+int num_players = 0;
+struct player_info players[7];
 
 int main() {
 
+  printf("Welcome to Spyfall\n");
+  printf("Connect clients to begin\n");
+  
   int sd, connection;
 
   sd = server_setup();
     
-  while (players < 5) {
+  while (num_players < 7) {
 
     connection = server_connect( sd );
-    player_ids[players] = connection;
-    players++;
+    players[num_players].usrid = connection;
+    //players++;
 
     int f = fork();
     if ( f == 0 ) {
 
       close(sd);
-      sub_server( connection );
-
+      sub_server( connection, num_players );
+      
       exit(0);
     }
     else {
@@ -45,16 +57,15 @@ int main() {
 }
 
 
-void sub_server( int sd ) {
-
-  char buffer[MESSAGE_BUFFER_SIZE];
+void sub_server( int sd, int player_id ) {
+   char buffer[MESSAGE_BUFFER_SIZE];
+   read(sd, players[player_id].username, USERNAME_SIZE);
+   //printf("%s connected", username);
   while (read( sd, buffer, sizeof(buffer) )) {
-
     printf("[SERVER %d] received: %s\n", getpid(), buffer );
     process( buffer );
     write( sd, buffer, sizeof(buffer));    
-  }
-  
+  }  
 }
 
 int choose_player(int sd) { 
@@ -68,14 +79,14 @@ void interrogate_player(int sd) {
 
   //Reads question from interrogator
   write(sd, "What would you like to ask?", 26);
-  read(sd, buffer, sizeof(buffer));
+  read(sd, buffer, MESSAGE_BUFFER_SIZE);
 
   //Write question to player being interrogated
   write(usrid, "Interrogator ask:", 16);
   write(usrid, buffer, sizeof(buffer));
 
   //Read response from player
-  read(usrid, buffer, sizeof(buffer));
+  read(usrid, buffer, MESSAGE_BUFFER_SIZE);
 }
 
 void process( char * s ) {
